701b9addf6a70b984db0aa426bd5612c8e5f7be1
Kiwathh <adnanfadhillah.id@gmail.com>
2025-12-12 10:58:42 +0700

    update jadwal match

--- src/controllers/sport.controller.js
+++ src/controllers/sport.controller.js
@@ -1,4 +1,5 @@
 const Sport = require("../models/sport.model");
+const db = require("../config/db");
 
 // Halaman list semua cabang olahraga
 exports.renderSports = async (req, res) => {
@@ -24,10 +25,24 @@ exports.renderSportDetail = async (req, res) => {
     if (!sport) {
       return res.status(404).send("Sport tidak ditemukan");
     }
-
+    const [matches] = await db.query(
+      `SELECT 
+      m.id, m.title, m.start_time, m.end_time,
+      ht.name AS home_team_name,
+      at.name AS away_team_name,
+      v.name AS venue_name
+   FROM matches m
+   LEFT JOIN teams ht ON ht.id = m.home_team_id
+   LEFT JOIN teams at ON at.id = m.away_team_id
+   LEFT JOIN venues v ON v.id = m.venue_id
+   WHERE m.sport_id = ?
+   ORDER BY m.start_time ASC`,
+      [sport.id]
+    );
     res.render("sports/detail", {
       title: sport.name,
       sport,
+      matches
     });
   } catch (err) {
     console.error(err);
