344629b7d407a6b88c77d339bb429784881c0b46
Dipzzz-2186 <silviahneneng2007@gmail.com>
2025-12-22 09:10:55 +0700

    update profile athletes

--- src/services/standings.service.js
+++ src/services/standings.service.js
@@ -17,37 +17,40 @@ async function getStandings({ sportId, mode }) {
     let rows = [];
 
     if (isPadel && mode === 'individual') {
-        [rows] = await db.query(`
-      SELECT
-        s.id,
-        t.name AS team_name,
-        sp.name AS sport_name,
-        (
-          SELECT COUNT(*)
-          FROM match_participants mp
-          JOIN matches m ON m.id = mp.match_id
-          WHERE mp.team_id = s.team_id
-            AND m.sport_id = s.sport_id
-            AND m.match_mode = 'individual'
-        ) AS total_match,
-        s.win,
-        s.loss,
-        s.game_win,
-        s.game_loss,
-        s.set_win,
-        s.set_loss,
-        (s.set_win - s.set_loss) AS set_diff,
-        s.score_for,
-        s.score_against,
-        (s.score_for - s.score_against) AS score_diff
-      FROM standings s
-      JOIN teams t ON t.id = s.team_id AND t.is_individual = 1
-      JOIN sports sp ON sp.id = s.sport_id
-      WHERE s.sport_id = ?
-      ORDER BY
-        s.win DESC,
-        (s.game_win - s.game_loss) DESC
-    `, [sportId]);
+      [rows] = await db.query(`
+        SELECT
+          s.id,
+          t.name AS team_name,
+          a.slug AS athlete_slug,
+          sp.name AS sport_name,
+          (
+            SELECT COUNT(*)
+            FROM match_participants mp
+            JOIN matches m ON m.id = mp.match_id
+            WHERE mp.team_id = s.team_id
+              AND m.sport_id = s.sport_id
+              AND m.match_mode = 'individual'
+          ) AS total_match,
+          s.win,
+          s.loss,
+          s.game_win,
+          s.game_loss,
+          s.set_win,
+          s.set_loss,
+          (s.set_win - s.set_loss) AS set_diff,
+          s.score_for,
+          s.score_against,
+          (s.score_for - s.score_against) AS score_diff
+        FROM standings s
+        JOIN teams t ON t.id = s.team_id AND t.is_individual = 1
+        JOIN athletes a ON a.individual_team_id = t.id
+        JOIN sports sp ON sp.id = s.sport_id
+        WHERE s.sport_id = ?
+        ORDER BY
+          s.win DESC,
+          (s.game_win - s.game_loss) DESC
+      `, [sportId]);
+
 
     } else if (isPadel) {
         [rows] = await db.query(`
